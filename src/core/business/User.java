a948a28ae1d65b0a77a26f8bddeb5bd9af95b09c
Baldemar Sepulveda <balde.sepu@pm.me>
2019-12-25 20:03:17 -0600

    Refactor for UserDAO

---
+++ src/core/business/User.java
@@ -0,0 +1,39 @@
+package core.business;
+
+public class User {
+	private String userName;
+	private String password;
+	private UserType type;
+	
+	public User() {}
+
+	public User(String userName, String password, UserType type) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.type = type;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public UserType getType() {
+		return type;
+	}
+
+	public void setType(UserType type) {
+		this.type = type;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
--- src/core/test/RegisterUserTest.java
+++ src/core/test/RegisterUserTest.java
@@ -5,13 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 
 import core.business.BankingSystem;
+import core.business.UserDAO;
 import core.business.UserType;
 
 public class RegisterUserTest {
 	@Test
 	public void registerUser() {
 		BankingSystem system = BankingSystem.getInstance();
-		String userName = "micheal";
+		String userName = "michealb";
 		String password = "lookhere";
 		UserType type = UserType.Employee;
 		boolean result = system.registerUser(userName, password, type);
