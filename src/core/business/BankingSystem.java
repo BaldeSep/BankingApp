ee078a9d605dd7e8a5dd1a9c40154adf81182386
Baldemar Sepulveda <balde.sepu@pm.me>
2019-12-25 21:16:04 -0600

    Refactor Creation Of BankAccout And Added InitialBalance

--- src/core/business/BankingSystem.java
+++ src/core/business/BankingSystem.java
@@ -3,23 +3,29 @@ package core.business;
 public class BankingSystem {
 	private static final BankingSystem bankingSystem = new BankingSystem();
 	private UserDAO userDAO;
+	private BankAccountDAO bankAccountDAO;
 	public BankingSystem() {
 		userDAO = new UserDAO();
+		bankAccountDAO = new BankAccountDAO();
 	}
 	
 	public static BankingSystem getInstance() {
 		return bankingSystem;
 	}
 	
-	public boolean registerUser(String userName, String password, UserType type) {
+	public boolean registerUser(final String userName, final String password, final UserType type) {
 		return userDAO.registerUser(userName, password, type);
 	}
 	
-	public boolean verifyUserCredentials(String userName, String password) {
+	public boolean verifyUserCredentials(final String userName, final String password) {
 		return userDAO.verifyUserCredentials(userName, password);
 	}
 
-	public boolean createBankAccount(String userName) {
-		return userDAO.createBankAccount(userName);
+	public boolean createBankAccount(final String userName) {
+		return bankAccountDAO.createBankAccount(userName);
+	}
+	
+	public boolean createBankAccount(final String userName,final double initialBalance ) {
+		return bankAccountDAO.createBankAccount(userName, initialBalance);
 	}
 }
--- src/core/test/CreateBankAccountTest.java
+++ src/core/test/CreateBankAccountTest.java
@@ -7,11 +7,19 @@ import org.junit.Test;
 import core.business.BankingSystem;
 
 public class CreateBankAccountTest {
-	@Test
 	public void createBankAccount() {
 		BankingSystem system = BankingSystem.getInstance();
-		String userName = "loki";
+		String userName = "balde";
 		boolean accountCreated =  system.createBankAccount(userName);
+		assertEquals(true, accountCreated);
+	}
+	
+	@Test
+	public void createBankAccountWithInitialBalance() {
+		BankingSystem system = BankingSystem.getInstance();
+		String userName = "killian";
+		double initialBalance = -50.00;
+		boolean accountCreated =  system.createBankAccount(userName, initialBalance);
 		assertEquals(true, accountCreated);
 	}
 		
