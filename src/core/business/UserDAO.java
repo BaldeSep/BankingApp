0ad7f9e373e66a199940babbda805a3a21fece06
Baldemar Sepulveda <balde.sepu@pm.me>
2019-12-25 20:16:34 -0600

    Login User Unit Test

--- src/core/business/UserDAO.java
+++ src/core/business/UserDAO.java
@@ -2,6 +2,7 @@ package core.business;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
@@ -11,6 +12,7 @@ public class UserDAO {
 	
 	public UserDAO() {}
 	
+	// Register User and add them into the Database
 	public boolean registerUser(String userName, String password, UserType type) {
 		boolean userInserted = false;
 		try(Connection connection = OracleDBConnection.getInstance()) {
@@ -34,6 +36,33 @@ public class UserDAO {
 		}
 		return userInserted;
 	}
+	
+	// Verify If The User Exists Within the database
+	public boolean verifyUserCredentials(String userName, String password) {
+		boolean userExists = false;
+		
+		try(Connection connection = OracleDBConnection.getInstance()){
+			// Create SQL Command
+			String queryUser = "Select user_name From Users Where user_name = ? And password = ?";
+			PreparedStatement queryUserStatement = connection.prepareStatement(queryUser);
+			// Add Dynamic Data
+			queryUserStatement.setString(1, userName);
+			queryUserStatement.setString(2, password);
+			// Get Results of Query
+			ResultSet results = queryUserStatement.executeQuery();
+			// If there is a result then the user exists
+			if(results.next()) {
+				userExists = true;
+			}
+		}catch(ClassNotFoundException e) {
+			log.error(e);
+		}catch(SQLException e) {
+			log.error(e);
+		}
+		
+		return userExists;
+	}
+	
 	
 	
 }
