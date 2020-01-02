54694a41071446c050f3604637a179cb7ce59b03
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-01 21:41:18 -0600

    All Exceptions Caught In Main

--- src/com/banking/main/Main.java
+++ src/com/banking/main/Main.java
@@ -1,11 +1,19 @@
 package com.banking.main;
 
+import org.apache.log4j.Logger;
+
 import com.banking.menus.MainMenu;
+import com.banking.menus.QuitMenu;
 
 public class Main {
-
+	private static final Logger log = Logger.getLogger(Main.class);
 	public static void main(String[] args) {
-		MainMenu.getMenu().presentMenu();
+		try{
+			MainMenu.getMenu().presentMenu();
+		}catch(Exception e) {
+			log.fatal("A Fatal Error Occured During The Excution Of The Program...");
+			QuitMenu.getMenu().presentMenu();
+		}
 	}
 
 }
